e0d29d8fd7acacf4e7577bcc6a947e7af55ab439
aldafernandez <aldana.fernandez@outlook.com.ar>
2025-01-07 11:31:57 -0300

    js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const nav = document.querySelector('#nav');
 const abrir = document.querySelector('#open-menu');
 const cerrar = document.querySelector('#close-menu');
+const btn = document.querySelector('#btnJs');
+
+
 
 abrir.addEventListener("click", () => {
     nav.classList.remove('nav');
     nav.classList.add('nav-visible');
+    btn.classList.add('btnJs');
 })
 
 cerrar.addEventListener("click", () => {
